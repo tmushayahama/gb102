640e7d337382a9efa07f60ec3c28ebb99dbabd4b
tmushayahama <tmushayahama@gmail.com>
2015-11-16 03:31:37 -0800

    INT: Edit the checkilist item

--- public/modules/skills/controllers/modals/SkillTodoCtrl.js
+++ public/modules/skills/controllers/modals/SkillTodoCtrl.js
@@ -32,10 +32,7 @@ angular.module("app.skills").controller('SkillTodoCtrl',
           vm.todoId = skillTodoData.todo_id;
           vm.editDecriptionMode = false;
           vm.addChecklistMode = false;
-          vm.skillTodoBackUp;
-          vm.skillTodo;
 
-          vm.todoChecklist = [];
           vm.todoFormDisplay = false;
 
 
@@ -53,20 +50,6 @@ angular.module("app.skills").controller('SkillTodoCtrl',
           vm.close = function () {
            $uibModalInstance.dismiss('cancel');
           };
-
-          vm.enterEditDescriptionMode = function () {
-           vm.editDecriptionMode = true;
-           vm.skillTodoBackUp = vm.skillTodo;
-          }
-
-          vm.enterAddChecklistMode = function () {
-           vm.addChecklistMode = true;
-           vm.skillTodoBackUp = vm.skillTodo;
-          }
-
-          vm.cancelEditDescriptionMode = function () {
-           vm.editDecriptionMode = false;
-          }
 
           vm.cancelAddChecklistMode = function () {
            vm.addChecklistMode = false;
@@ -138,13 +121,6 @@ angular.module("app.skills").controller('SkillTodoCtrl',
            //$scope.user = angular.copy($scope.master);
           };
 
-
-
-
-
-
-          vm.editedTodo = null;
-
           $scope.$watch(angular.bind(this, function () {
            return vm.todoChecklist;
           }), function () {
@@ -153,87 +129,6 @@ angular.module("app.skills").controller('SkillTodoCtrl',
            vm.allChecked = !vm.remainingCount;
            //TodoChecklistService.put(vm.todoChecklist);
           }, true);
-          /*
-           $scope.$watch(angular.bind(this, function () {
-           return vm.location.path();
-           }), function (path) {
-           vm.statusFilter = (path === '/active') ?
-           {completed: false} : (path === '/completed') ?
-           {completed: true} : null;
-           });
-           */
-
-
-
-
-          vm.editTodo = function (todoChecklist) {
-           vm.editedTodo = todoChecklist;
-           // Clone the original todoChecklist to restore it on demand.
-           vm.originalTodo = angular.copy(todoChecklist);
-          };
-
-
-          vm.doneEditing = function (todoChecklist) {
-           vm.editedTodo = null;
-           todoChecklist.title = todoChecklist.title.trim();
-
-           if (!todoChecklist.title) {
-            vm.removeTodo(todoChecklist);
-           }
-          };
-
-          vm.revertEditing = function (todoChecklist) {
-           vm.todoChecklist[vm.todoChecklist.indexOf(todoChecklist)] = vm.originalTodo;
-           vm.doneEditing(vm.originalTodo);
-          };
-
-          vm.removeTodo = function (todoChecklist) {
-           vm.todoChecklist.splice(vm.todoChecklist.indexOf(todoChecklist), 1);
-          };
-
-
-          vm.clearDoneTodos = function () {
-           vm.todoChecklist = vm.todoChecklist = vm.todoChecklist.filter(function (val) {
-            return !val.completed;
-           });
-          };
-
-
-          vm.markAll = function (done) {
-           vm.todoChecklist.forEach(function (todoChecklist) {
-            todoChecklist.completed = done;
-           });
-          };
-
-          vm.items = ['item1', 'item2', 'item3'];
-
-
-          vm.openTodoChecklist = function (todoChecklist) {
-
-           var modalInstance = $uibModal.open({
-            animation: true,
-            templateUrl: 'todo-todo-modal.html',
-            controller: 'TodoChecklistCtrl as todoChecklistCtrl',
-            backdrop: 'static',
-            size: 'xl',
-            resolve: {
-             todoChecklistData: function () {
-              return todoChecklist;
-             }
-            }
-           });
-
-           modalInstance.result.then(function (selectedItem) {
-            $scope.selected = selectedItem;
-           }, function () {
-            $log.info('Modal dismissed at: ' + new Date());
-           });
-          };
-
-          $scope.toggleAnimation = function () {
-           $scope.animationsEnabled = !$scope.animationsEnabled;
-          };
-
 
           //--------init------
           vm.getSkillTodo(vm.skillId, vm.todoId);
