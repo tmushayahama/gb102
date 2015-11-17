8f02b0e830daa89da66dd6b674072715c23ceb29
tmushayahama <tmushayahama@gmail.com>
2015-11-17 04:25:53 -0800

    FIX: Removed the checklist from the notes

--- public/modules/skills/controllers/modals/SkillNoteCtrl.js
+++ public/modules/skills/controllers/modals/SkillNoteCtrl.js
@@ -1,6 +1,5 @@
 angular.module("app.skills").controller('SkillNoteCtrl',
         ['SkillNoteManager',
-         'SkillNoteChecklistManager',
          '$uibModalInstance',
          '$scope',
          '$state',
@@ -12,7 +11,6 @@ angular.module("app.skills").controller('SkillNoteCtrl',
          'skillNoteData',
          function (
                  SkillNoteManager,
-                 SkillNoteChecklistManager,
                  $uibModalInstance,
                  $scope,
                  $state,
@@ -26,20 +24,13 @@ angular.module("app.skills").controller('SkillNoteCtrl',
           vm.skillId = skillNoteData.skill_id;
           vm.skillNoteId = skillNoteData.id;
           vm.skillNoteManager = new SkillNoteManager();
-          vm.skillNoteChecklistManager = new SkillNoteChecklistManager();
 
 
           vm.noteId = skillNoteData.note_id;
-          vm.checklistFormVisible = false;
 
           vm.noteFormDisplay = false;
 
 
-          vm.defaultNoteChecklistData = {
-           noteId: vm.noteId,
-           privacy: 0
-          }
-          vm.newSkillNoteChecklistData = angular.copy(vm.defaultNoteChecklistData);
 
 
           vm.ok = function () {
@@ -77,67 +68,15 @@ angular.module("app.skills").controller('SkillNoteCtrl',
            }
           }
 
-          vm.getSkillNoteChecklist = function (noteId) {
-           vm.skillNoteChecklistManager.getSkillNoteChecklist(noteId).then(function (response) {
-           }, function (response) {
-            console.log(response);
-           });
-          };
+
 
           vm.showNoteForm = function () {
            vm.noteFormDisplay = true;
           };
 
-          vm.createSkillNoteChecklistItem = function (data) {
-           vm.skillNoteChecklistManager.createSkillNoteChecklistItem(data).then(function (response) {
-            vm.checklistFormVisible = false;
-            vm.newSkillNoteChecklistData = angular.copy(vm.defaultNoteChecklistData);
-           }, function (response) {
-            console.log(response);
-           });
-          };
 
-          vm.editSkillNoteChecklistItem = function (data) {
-           vm.skillNoteChecklistManager.editSkillNoteChecklistItem(data).then(function (response) {
-            vm.newSkillNoteChecklistData = angular.copy(vm.defaultNoteChecklistData);
-           }, function (response) {
-            console.log(response);
-           });
-          };
-
-          vm.editSkillNoteChecklistItemSections = {
-           title: function (checklistId, title) {
-            var skillNoteChecklistItemData = {
-             checklistId: checklistId,
-             title: title
-            };
-            vm.editSkillNoteChecklistItem(skillNoteChecklistItemData);
-           }
-          }
-
-
-
-          vm.cancelChecklistForm = function (form) {
-           vm.checklistFormVisible = false;
-           vm.newSkillNoteChecklistData = angular.copy(vm.defaultNoteChecklistData);
-           if (form) {
-            form.$setPristine();
-            form.$setUntouched();
-           }
-           //$scope.user = angular.copy($scope.master);
-          };
-
-          $scope.$watch(angular.bind(this, function () {
-           return vm.skillNoteChecklistManager.skillNoteChecklist;
-          }), function () {
-           //vm.remainingCount = filterFilter(noteChecklist, {completed: false}).length;
-           vm.doneCount = vm.skillNoteChecklistManager.skillNoteChecklist.length - vm.remainingCount;
-           vm.allChecked = !vm.remainingCount;
-           //NoteChecklistService.put(vm.noteChecklist);
-          }, true);
 
           //--------init------
           vm.getSkillNote(vm.skillId, vm.noteId);
-          vm.getSkillNoteChecklist(vm.noteId);
          }
         ])
