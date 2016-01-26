a0671941f6ad3a461dd5c2ba0b3499ae71aa36e4
tmushayahama <tmushayahama@gmail.com>
2016-01-26 08:05:36 -0800

    FIX: the UI for the navbar

--- public/modules/app/controllers/AppsCtrl.js
+++ public/modules/app/controllers/AppsCtrl.js
@@ -41,6 +41,40 @@ var appsCtrl = function (
   return newSupes;
  }
 
+ vm.refreshResults = function ($select) {
+  var search = $select.search,
+          list = angular.copy($select.items),
+          FLAG = -1;
+  //remove last user input
+  list = list.filter(function (item) {
+   return item.id !== FLAG;
+  });
+
+  if (!search) {
+   //use the predefined list
+   $select.items = list;
+  } else {
+   //manually add user input and set selection
+   var userInputItem = {
+    id: FLAG,
+    description: search
+   };
+   $select.items = [userInputItem].concat(list);
+   $select.selected = userInputItem;
+  }
+ }
+
+ vm.clearSearch = function ($event, $select) {
+  //stops click event bubbling
+  $event.stopPropagation();
+  //to allow empty field, in order to force a selection remove the following line
+  //$select.selected = undefined;
+  //reset search query
+  $select.search = undefined;
+  //focus and open dropdown
+  $select.activate();
+ }
+
  vm.searchSuggestion = [
   'Superman',
   'Batman',
