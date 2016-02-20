ddf5edc367a102d2bc37d05a50efc9d8159abee5
tmushayahama <tmushayahama@gmail.com>
2016-02-20 13:38:21 -0800

    ENH: Added the favicons

--- resources/views/index.php
+++ resources/views/index.php
@@ -9,22 +9,32 @@
   <title>SkillSection</title>
 
   <!-- Favicons -->
-  <link href='http://fonts.googleapis.com/css?family=Schoolbell&v1' rel='stylesheet' type='text/css'>
-  <link href="images/favicon/favicon.png" rel="shortcut icon">
-  <link href="images/favicon/apple-touch-icon-57-precomposed.png" rel="apple-touch-icon">
-  <link href="images/favicon/apple-touch-icon-72-precomposed.png" rel="apple-touch-icon" sizes="72x72">
-  <link href="images/favicon/apple-touch-icon-144-precomposed.png" rel="apple-touch-icon" sizes="114x114">
+  <link rel="apple-touch-icon" sizes="57x57" href="public/img/favicons//apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="public/img/favicons//apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="public/img/favicons//apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="public/img/favicons//apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="public/img/favicons//apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="public/img/favicons//apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="public/img/favicons//apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="public/img/favicons//apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="public/img/favicons//apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="public/img/favicons//android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="public/img/favicons//favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="public/img/favicons//favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="public/img/favicons//favicon-16x16.png">
+  <link rel="manifest" href="public/img/favicons//manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
 
+  <link href='http://fonts.googleapis.com/css?family=Schoolbell&v1' rel='stylesheet' type='text/css'>
   <link href="public/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
   <link href="public/gb-icons/gb-icons.css" rel="stylesheet" type="text/css"/>
   <link href="public/css/gb-sass/stylesheets/main.css" rel="stylesheet" type="text/css"/>
+  <link href="../../public/css/gb-sass/stylesheets/gb-themes/app-theme-skill.css" rel="stylesheet" type="text/css"/>
+
   <link href="public/css/gb-sass/stylesheets/gb-landing-page.css" rel="stylesheet" type="text/css"/>
 
-
-  <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
  </head>
  <body class="">
   <div id="loadingProgressG">
