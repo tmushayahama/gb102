c8fcad215addbb49b9f5f791b9ebc6bd64349f0b
tmushayahama <tmushayahama@gmail.com>
2016-02-29 04:12:51 -0800

    ENH: Added the material design on the main menu

--- public/modules/app/controllers/apps.ctrl.js
+++ public/modules/app/controllers/apps.ctrl.js
@@ -23,8 +23,6 @@ var appsCtrl = function (
  var _selected;
 
  $scope.selected = undefined;
- vm.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
- // Any function returning a promise object can be used to load values asynchronously
 
  vm.searchSrv = new SearchSrv();
 
@@ -34,11 +32,8 @@ var appsCtrl = function (
   };
   return vm.searchSrv.simpleSearchSuggestion(searchData)
           .then(function (response) {
-           var results = response.data.map(function (item) {
-            return item.title;
-           });
-           vm.suggestions = results;
-           return results;
+           //vm.suggestions = response.data;
+           return response.data;
           });
  };
  //vm.getSearchResults('so');
