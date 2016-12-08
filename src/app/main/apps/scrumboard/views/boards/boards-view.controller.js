c3b9917cb6a018ffc4c6446c589b65d1f3721b16
tmushayahama <tmushayahama@gmail.com>
2016-12-07 18:49:56 -0800

    [ENH - UI] - Prettified the Explorer page

--- src/app/main/apps/scrumboard/views/boards/boards-view.controller.js
+++ src/app/main/apps/scrumboard/views/boards/boards-view.controller.js
@@ -7,13 +7,23 @@
          .controller('BoardsViewController', BoardsViewController);
 
  /** @ngInject */
- function BoardsViewController(BoardList)
+ function BoardsViewController(BoardList, $rootScope)
  {
   var vm = this;
 
   // Data
   vm.boardList = BoardList;
 
+  angular.forEach(vm.boardList.apps, function (app, key) {
+   angular.forEach(app.components, function (component, key) {
+    if (component.component_picture_url || component.component_picture_url === 'default.png') {
+     component.component_placeholder_style = {background: $rootScope.generateBackgroundPattern()};
+    }
+   });
+  });
+
+  $rootScope.headerStyle = {background: $rootScope.generateBackgroundPattern()};
+  $rootScope.appName = "Explorer";
   // Methods
 
   //////////
